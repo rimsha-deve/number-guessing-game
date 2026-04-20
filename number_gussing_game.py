48425df30e6b28069200379b5cc63b62c8617dba
Rimsha <youremail@example.com>
2026-04-20 05:46:14 -0400

    Add number guessing game code

---
+++ number_gussing_game.py
@@ -0,0 +1,21 @@
+'''We are going to write a program that generates a random number and asks the user to
+guess it.
+If the player’s guess is higher than the actual number, the program displays “Lower
+number please”. Similarly, if the user’s guess is too low, the program prints “higher
+number please” When the user guesses the correct number, the program displays the
+number of guesses the player used to arrive at the number.
+Hint: Use the random module.'''
+import random
+n=random.randint(1,100)
+a=-1
+guesses=0
+while a!=n:
+    guesses+=1
+    a=int(input("Guess the number :"))
+    if a>n:
+        print("Enter lower number:")
+    if a<n:
+        print("Enter higher number:")
+
+
+print(f"you have guess the number {n} correctly in {guesses} attempts")
